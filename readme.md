69212c06dbe87792cecebcd7996b24d784187059
Chris Anderson <chrande@outlook.com>
2015-12-22 16:57:39 -0800

    clean up connection logic

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ A simple example of accessing a SQL Server from Node on Azure WebApps
 
 ## What it does
 
-This connects to a SQL DB in a connection string named `SQL_DB_CONNECTION_STRING` and runs `SELECT 'foobar'`, then prints the result to stdout.
+This connects to a SQL DB in an ADO.NET connection string named `SQLAZURECONNSTR_SQLDB` and runs `SELECT 'foobar'`, then prints the result to stdout.
 It runs every minute, thanks to the schedule setting in the settings.json. This only works in Standard with Always On turned on. If you need CRON Jobs outside of Standard tier, use Azure Scheduler.
 
 ## How to deploy
@@ -13,7 +13,10 @@ There are many ways to deploy a WebJob: this one is simple, but manual.
 1. Clone repo
 2. Run npm install
 3. zip up all contents of folder
-4. Deploy via the portal as On Demand (Triggered) job
+4. On a new or existing App Service App (Web, Mobile, API), set the `SQLDB` as a Connection String (`SQLAZURECONNSTR` is prepended to SQL Connection Strings; you can also use `SQLAZURECONNSTR_SQLDB` as an App Setting)
+4. Deploy via the portal as On Demand (Triggered) job to the same App Service App.
+
+Once you've finished, you can click on the logs link in the portal or go to `{sitename}.scm.azurewebsites.net/AzureJobs` to see the results.
 
 ## Important Notes
 
--- run.js
+++ run.js
@@ -1,12 +1,51 @@
-var mssql = require('node-mssql');
+var mssql = require('mssql');
 
-mssql.connect(process.env['SQL_DB_CONNECTION_STRING']).then(function() {
-    // Query
-    new mssql.Request().query("select 'foobar'").then(function(recordset) {
-        console.dir(recordset);
-    }).catch(function(err) {
+console.log('Running SQL Query');
+
+mssql.connect(process.env['SQLAZURECONNSTR_SQLDB'], function(err) {
+    // ... error checks
+    
+    if(err) {
+        console.log('Connection broke');
+        return console.log(err);
+    }
+
+    var request = new mssql.Request();
+    request.stream = true; // You can set streaming differently for each request
+    request.query("select 'foobar' as result"); // or request.execute(procedure);
+
+    request.on('recordset', function(columns) {
+        // Emitted once for each recordset in a query
+    });
+
+    request.on('row', function(row) {
+        // Emitted for each row in a recordset
+        console.log(row);
+    });
+
+    request.on('error', function(err) {
+        // May be emitted multiple times
+        console.log('Query broke');
         console.log(err);
     });
-}).catch(function(err) {
-    console.log(err);
+
+    request.on('done', function(returnValue) {
+        // Always emitted as the last one
+        console.log('Completed Query');
+        process.exit(0);
+    });
+});
+
+mssql.on('error', function(err) {
+    // ... error handler
+    console.log('Could not connect to SQL DB');
+    process.exit(1);
+});
+
+process.on('uncaughtException', function(err) {
+    console.log('Unhandled Exception', err);
+});
+
+process.on('exit', function(code) {
+  console.log('About to exit with code:', code);
 });
